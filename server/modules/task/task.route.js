e419ba179348f19f96cb3290e00a160d851d6b15
zohaibkibria <zohaib.k@cloudbloom.io>
2023-02-10 14:42:04 +0530

    github issues

--- server/modules/task/task.route.js
+++ server/modules/task/task.route.js
@@ -6,6 +6,7 @@ const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/:taskId', web3Auth, taskCtrl.getById);
 router.post('/', web3Auth, taskCtrl.create);
+router.post('/storeGithubIssues', web3Auth, taskCtrl.storeGithubIssues);
 router.post('/draft', web3Auth, taskCtrl.draftTask);
 router.patch('/:taskId/apply', web3Auth, taskCtrl.applyTask);
 router.patch('/:taskId/assign', web3Auth, taskCtrl.assignTask);
