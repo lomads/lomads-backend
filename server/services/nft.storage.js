b1bdd56419317835f6bc70142178737e3efcb8e1
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-03-15 22:09:03 +0530

    fix

--- server/services/nft.storage.js
+++ server/services/nft.storage.js
@@ -2,18 +2,30 @@ const config = require('@config/config');
 const axios = require('axios');
 const moment = require('moment')
 const mime = require('mime')
+const fetch = require("node-fetch");
 const _ = require('lodash')
 const { NFTStorage, File } = require("nft.storage")
 
 const client = new NFTStorage({ token: config.nftStorage })
 
+
+const getImage = async (imageOriginUrl) => {
+    const r = await fetch(imageOriginUrl)
+    if (!r.ok) {
+      throw new Error(`error fetching image: [${r.statusCode}]: ${r.status}`)
+    }
+    return new File([r.blob()], "fileNameGoesHere.png", { type: "image/png" })
+  }
+
 const storeNFTMetadata = async (metadata, externalTokenURI) => {
     try {
         const contentType = mime.getType(metadata.image);
         const ext = mime.getExtension(contentType)
-        const image_blob = await axios.get(metadata?.image, { responseType: 'blob' }).then(response => {
-           return new File([response.data], `${moment.unix()}.${ext}`, { type: contentType } );       
-        });
+        console.log(metadata?.image, contentType)
+        // const image_blob = await axios.get(metadata?.image, { responseType: 'blob' }).then(response => {
+        //    return new File([response.data], `${moment().unix()}.${ext}`, { type: contentType} );       
+        // });
+        const image_blob = await getImage(metadata?.image)
         const nft = {
             image: image_blob,
             name: metadata?.name,
