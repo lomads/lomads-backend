c0f1f6c4370aa3964cd8497af313238037ccab04
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-03-24 17:00:48 +0530

    fix

--- server/services/smartContract.js
+++ server/services/smartContract.js
@@ -2,7 +2,7 @@ var config = require('@config/config');
 const { ethers } = require('ethers')
 
 
-const getSignature = async ({ chainId, contract, tokenId }) => {
+const getSignature = async ({ chainId, contract, tokenId, payment = '' }) => {
 
     let signer = new ethers.Wallet(config.walletPrivateKey)
     const domain = {
@@ -13,10 +13,11 @@ const getSignature = async ({ chainId, contract, tokenId }) => {
     }
     const types = {
         Web3Struct: [
-            { name: "id", type: "uint256" }
+            { name: "id", type: "uint256" },
+            { name: "payment", type: "string" }
         ]
     }
-    const signature = signer.signTypedData(domain, types, { id: tokenId })
+    const signature = signer.signTypedData(domain, types, { id: tokenId, payment })
     return signature
 }
 
