c00d93a95b4d5d3ff24bc28d8682e2c3bddd7075
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-03-28 22:41:34 +0530

    admin login

--- server/modules/auth/auth.route.js
+++ server/modules/auth/auth.route.js
@@ -3,13 +3,14 @@ const web3Auth = require('@server/services/web3Auth');
 const createAccount = require('@server/services/createAccount');
 const router = express.Router(); // eslint-disable-line new-cap
 const authCtrl = require('./auth.controller');
+const web3AdminAuth = require('@server/services/web3AdminAuth');
 
 router.get('/me', web3Auth, (req, res) => {
   return res.status(200).json(req.user)
 });
 
 router.patch('/me', web3Auth, authCtrl.update);
-
+router.post('/admin', web3AdminAuth, createAccount);
 router.post('/create-account', createAccount);
 router.post('/create-account-aikon', authCtrl.createAccountAikon);
 
