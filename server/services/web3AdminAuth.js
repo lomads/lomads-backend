6986404f69bff8af1e2b49f5b1ed4369d603db39
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-03-28 22:32:38 +0530

    admin login

--- server/services/web3AdminAuth.js
+++ server/services/web3AdminAuth.js
@@ -2,7 +2,7 @@ const web3AdminAuth = async (req, res, next) => {
     try {
         if(req?.user && req?.user?.platformRole && req?.user?.platformRole === 'admin')
             next();
-        return res.status(500).json({ message: 'Unauthorised' })
+        return res.status(401).json({ message: 'Unauthorised' })
     } catch (e) {
         console.log(e)
         return res.status(500).json({ message: 'Something went wrong' })
