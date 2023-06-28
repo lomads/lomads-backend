11e1eb1f334225d6d1d8aa2f8382f239f2d01d80
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-06-28 19:45:55 +0530

    bug fix

--- server/modules/safe/safe.controller.js
+++ server/modules/safe/safe.controller.js
@@ -1,11 +1,25 @@
 const Safe = require('@server/modules/safe/safe.model');
+const Member = require('@server/modules/member/member.model');
 const ObjectId = require('mongodb').ObjectID;
 
 
 const syncSafe = async (req, res) => {
     const { address } = req.params 
+    const { owners, ...others } = req.body;
     try {
-        const response = await Safe.updateMany({ address }, { $set: { ...req.body } })
+        let members = []
+        for (let index = 0; index < owners.length; index++) {
+            const owner = owners[index];
+            const filter = { wallet: { $regex: new RegExp(`^${owner}$`, "i") } }
+            let m = await Member.findOne(filter);
+            if (!m) {
+                m = new Member({ wallet: toChecksumAddress(owner), name: '' })
+                m = await m.save();
+            }
+            members.push(m)
+        }
+
+        const response = await Safe.updateMany({ address }, { $set: { ...others }, $addToSet: { owners: members.map(m => m._id) } })
         return res.status(200).json({ message: 'Success' })
     }
     catch (e) {
