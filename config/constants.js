adef653d886f7a4494da2352bae7785b2560ba74
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-07-28 19:01:11 +0530

    bug fix

--- config/constants.js
+++ config/constants.js
@@ -2,16 +2,9 @@ const config = require('@config/config')
 
 const SupportedChainId = {
     MAINNET: 1,
-    ROPSTEN: 3,
-    RINKEBY: 4,
     GOERLI: 5,
-    KOVAN: 42,
-    ARBITRUM_ONE: 42161,
-    ARBITRUM_RINKEBY: 421611,
-    OPTIMISM: 10,
-    OPTIMISTIC_KOVAN: 69,
     POLYGON: 137,
-    POLYGON_MUMBAI: 80001,
+    CELO: 42220
   }
 
   const SBT_DEPLOYER_ADDRESSES  = {
@@ -22,26 +15,24 @@ const SupportedChainId = {
   const GNOSIS_API_ENDPOINT  = {
     [SupportedChainId.GOERLI]: "https://safe-transaction-goerli.safe.global",
     [SupportedChainId.POLYGON]: 'https://safe-transaction-polygon.safe.global',
-    [SupportedChainId.MAINNET]: 'https://safe-transaction-mainnet.safe.global'
+    [SupportedChainId.MAINNET]: 'https://safe-transaction-mainnet.safe.global',
+    [SupportedChainId.CELO]: 'https://safe-transaction-celo.safe.global'
   } 
 
   const INFURA_NETWORK_URLS = (INFURA_KEY) => {
     return {
       [SupportedChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
       [SupportedChainId.GOERLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.OPTIMISM]: `https://optimism-mainnet.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.OPTIMISTIC_KOVAN]: `https://optimism-kovan.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.ARBITRUM_ONE]: `https://arbitrum-mainnet.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.ARBITRUM_RINKEBY]: `https://arbitrum-rinkeby.infura.io/v3/${INFURA_KEY}`,
       [SupportedChainId.POLYGON]: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
-      [SupportedChainId.POLYGON_MUMBAI]: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
+      [SupportedChainId.CELO]: `https://celo-mainnet.infura.io/v3/${INFURA_KEY}`
     }
   }
 
   const NETWORK_SCAN_LINKS = {
+    [SupportedChainId.MAINNET]: { 
+      baseUrl: `https://api.etherscan.io/`,
+      apiKey: config.etherScanKey
+    },
     [SupportedChainId.GOERLI]: { 
       baseUrl: `https://api-goerli.etherscan.io/`,
       apiKey: config.etherScanKey
@@ -49,6 +40,10 @@ const SupportedChainId = {
     [SupportedChainId.POLYGON]: { 
       baseUrl: `https://api.polygonscan.com/`,
       apiKey: config.polyScanKey
+    },
+    [SupportedChainId.CELO]: { 
+      baseUrl: `https://api.celoscan.com/`,
+      apiKey: config.celoScanKey
     },
   }
 
